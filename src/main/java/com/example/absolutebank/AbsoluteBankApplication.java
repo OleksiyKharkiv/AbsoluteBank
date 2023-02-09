92261ac092a598146eacbc47049f044e62079578
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-02-09 10:57:02 +0100

    Absolute Start

---
+++ src/main/java/com/example/absolutebank/AbsoluteBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.absolutebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsoluteBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsoluteBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/absolutebank/AbsoluteBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.absolutebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsoluteBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
